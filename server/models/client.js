68d9430a3a818fbc02b639eb9901578662a1ed70
Aniruddh Bharadwaj <aniruddh@usc.edu>
2017-10-07 02:51:42 -0700

    mongoose to fkin datastore

--- server/models/client.js
+++ server/models/client.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
+// import mongoose from 'mongoose';
+// const Schema = mongoose.Schema;
 
-const Client = new Schema({
-  phoneNumber: String,
-  zipcode: String,
-  subscribedRestaurants: [Schema.Types.ObjectId]
-}, {
-  timestamps: true
-});
+// const Client = new Schema({
+//   phoneNumber: String,
+//   zipcode: String,
+//   subscribedRestaurants: [Schema.Types.ObjectId]
+// }, {
+//   timestamps: true
+// });
 
-export default mongoose.model('Client', Client);
+// export default mongoose.model('Client', Client);
--- server/models/index.js
+++ server/models/index.js
@@ -1,19 +1,19 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+// const basename = path.basename(module.filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = require('../config/config')[env];
 
-const db = {};
+// const db = {};
 
-fs.readdirSync(__dirname)
-  .filter(function(file){
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file){
-    const model = require(path.join(__dirname, file));
-    db[model.modelName] = model;
-  });
+// fs.readdirSync(__dirname)
+//   .filter(function(file){
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(function(file){
+//     const model = require(path.join(__dirname, file));
+//     db[model.modelName] = model;
+//   });
 
-export default db;
+// export default db;
