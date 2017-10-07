efc413ed9baf517abef4f64cba1cfc444bc672bb
alexander-lee <thealexanderlee@me.com>
2017-10-07 02:34:34 -0700

    edit for restaurant

---
+++ server/routes/restaurant.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import { Restaurant } from '../models';
+
+const router = express.Router();
+
+/*
+  Request Body: {
+    location: String,
+    pickupTimes: [[{}]],
+  }
+*/
+router.put('/edit/:id', async (req, res) => {
+  const body = req.body;
+
+  try {
+    // Error Handling
+    if (body.pickupTimes.length !== 7) {
+      throw new Error('pickupTimes needs to have 7 entries');
+    }
+
+    for (day of body.pickupTimes) {
+      for (interval of day) {
+        if (!interval.hasOwnProperty('startDate') || !interval.hasOwnProperty('endDate')) {
+          throw new Error('pickupTimes needs to have intervals with startDate and endDate');
+        }
+      }
+    }
+
+    const restaurant = await Restaurant.findById(req.params.id);
+    restaurant.location = body.location;
+    restaurant.pickupTimes = body.pickupTimes;
+    await restaurant.save();
+
+    res.status(200).send({
+      restaurant
+    });
+  }
+  catch (error) {
+    res.status(400).send({
+      error
+    });
+  }
+});
