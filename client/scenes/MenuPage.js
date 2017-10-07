b9fe8236dfea703a37996ee135dbf75c9be4a60d
peniqliotuv <jerrytsu@usc.edu>
2017-10-07 02:13:48 -0700

    menu routes done

--- client/scenes/MenuPage.js
+++ client/scenes/MenuPage.js
@@ -5,63 +5,12 @@ import s from '../styles/Menu.scss';
 
 class MenuPage extends Component {
 
-    static defaultProps = {
-        menuItems: [
-            {
-                name: 'Pizza',
-                servings: 10,
-            },
-            {
-                name: 'Apples',
-                servings: 14,
-            },
-            {
-                name: 'Pasta',
-                servings: 2,
-            },
-            {
-                name: 'Tacos',
-                servings: 90,
-            },
-            {
-                name: 'Babu',
-                servings: 99999,
-            },
-        ],
-    };
-
-
-    state = {
-
-    };
-
-    handleClick = () => {
-        this.props.router.push('/menu/edit');
-    };
-
-    renderMenuItems = () => {
-        return this.props.menuItems.map((item, index) => {
-            return (
-                <MenuItem
-                    name={item.name}
-                    servings={item.servings}
-                    key={index}
-                />
-            );
-        });
-    };
-
     render() {
         return (
             <div>
                 <Navbar router={this.props.router}/>
                 <p>Menu Page</p>
-                <div className={s.menuItemContainer}>
-                    { this.renderMenuItems() }
-                </div>
-                <div onClick={this.handleClick} className={s.editMenuButton}>
-                    Edit Menu
-                </div>
+                { this.props.children }
             </div>
         );
     }
