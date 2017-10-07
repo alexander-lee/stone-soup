5109afb7ad149c48d859739fb97ba16f1fc3adae
Clifford Lee <leecliff@usc.edu>
2017-10-07 05:21:45 -0700

    stopping to study and also because i can't figure async type checking fuck my life

--- server/routes/sms.js
+++ server/routes/sms.js
@@ -11,8 +11,7 @@ router.use(session({secret: 'not-secret-at-all'}));
 
 router.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
-  const state = req.session.state ? req.session.state : (checkIfRegistered(req.body.From) ? 'menu' : 'unregistered');
-  console.log(state);
+  const state = req.session.state ? req.session.state : (checkIfRegistered(req.body.From) ? 'menu_restaurants' : 'unregistered');
 
   switch(state) {
     case 'unregistered':
@@ -20,19 +19,65 @@ router.post('/sms', (req, res) => {
       req.session.state = 'zip';
       break;
     case 'zip':
-      // const zip = req.body.something
-      // validate(zipcode)
-      twiml.message('Do you have any dietary restrictions?\n1: Vegetarian\n2: Diabetic\n3: Peanut/Soy Allergies');
-      req.session.state = 'dietary_restrictions';
+      if (/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(req.body.Body.trim())) {
+        req.session.zip = req.body.Body;
+        twiml.message('Do you have any dietary restrictions?\n0: None\n1: Vegetarian\n2: Diabetic\n3: Peanut/Soy Allergies\nIf you have multiple, enter the numbers with commas in between (e.g. 1,2,3)');
+        req.session.state = 'dietary_restrictions';
+      } else {
+        twiml.message('That wasn\'t a valid zip code, try again');
+      }
       break;
     case 'dietary_restrictions':
-      // dietary restrictions locked to account
-      twiml.message('Great! You\'ve completed registration!')
-      req.session.state = 'menu';
+      // TODO promises/async/await give me depression i just want to validate
+      let restrictions = req.body.Body.split(',');
+      let ans = validateDietaryRestrictions(restrictions);
+      console.log(ans);
+      if (ans) {
+        twiml.message(
+        'Here\'s a list of participating restaurants around your location. Choose 5 restaurants to subscribe to:\nLISTGOESHERE')
+        req.session.state = 'subscribe';
+      } else {
+        twiml.message('Invalid dietary restriction input, follow the format (\'1\', \'1,2,3\', \'0\')');
+      }
       break;
-    case 'menu':
+    case 'subscribe':
+      // if valid choices
       // some function to generate menu based on user settings here
-      twiml.message('Today\'s Menu:\n1) A\n2) B\n3) C\n4) D\n5) E');
+      twiml.message('You\'re all set! Here\'s today\'s Menu:\n1) A\n2) B\n3) C\n4) D\n5) E');
+      req.session.state = 'menu_items';
+      break;
+    case 'menu_restaurants':
+      if (req.session.claimed) {
+        twiml.message('You\'ve already claimed a ticket today, come again tomorrow!');
+      } else {
+        twiml.message('Today\'s Menu:\n1) A\n2) B\n3) C\n4) D\n5) E');
+        req.session.state = 'menu_items';
+      }
+      break;
+    case 'menu_items':
+      // some work getting menu items based on phone number & restaurant choice
+      twiml.message('menu items here'); // can go back from here
+      req.session.state = 'order'
+      break;
+    case 'order':
+      // if go back
+      // req.session.state = 'menu_restaurants'
+      // else
+      // save restaurant & item for ticket gen
+      twiml.message('Confirm your order for menu_item from restaurant (Y/N)');
+      req.session.state = 'ticket';
+      break;
+    case 'ticket':
+      // if Y
+      const ticketCode = 's183dxY';
+      twiml.message('Here\'s your unique food ticket code: ' + ticketCode);
+      req.session.claimed = true;
+      req.session.state = 'menu_restaurants';
+      // function to consume a ticket from restaurant db
+      // if N
+      // twimlmessage('We\'ve cancelled this order! Text us again if you\'d like to start the process over');
+      // req.session.state = 'menu_restaurants'
+      break;
     default:
       break;
   }
@@ -44,6 +89,12 @@ router.post('/sms', (req, res) => {
 
 function checkIfRegistered(number) {
   return false;
+}
+
+function validateDietaryRestrictions(restrictions) {
+  Promise.all(restrictions.map((restriction) => {
+
+  }));
 }
 
 export default router;
